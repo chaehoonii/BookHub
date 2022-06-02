a6334749ada73af021bd83d87757c9b2301e977a
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-06-03 07:03:37 +0900

    [FEAT] 독서활동 기록 페이지 임시 구현 (테스트용) #3

---
+++ BookHub/src/main/java/multi/dokgi/bookhub/readinglog/service/IReadingLogService.java
@@ -0,0 +1,22 @@
+package multi.dokgi.bookhub.readinglog.service;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+import multi.dokgi.bookhub.readinglog.dto.ReadingLogDTO;
+
+/**
+ * @author GhostFairy
+ *
+ */
+public interface IReadingLogService {
+
+	public JSONObject getBookInfo(String isbn);
+
+	public int writeReadingLog(String userId, String bookISBN, int startPage, int endPage, String summary,
+			String readDate, String readComplete);
+
+	public List<ReadingLogDTO> getRecentReadingLog(String userId);
+
+}
