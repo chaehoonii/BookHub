5526d1c9db138d4cb06faa4add0532f6529c7759
Seongil, Yoon <dbs3346@gmail.com>
2022-06-03 11:12:21 +0900

    [FEAT] ServletInitializer수정 #8

--- BookHub/src/main/java/multi/dokgi/bookhub/HomeController.java
+++ BookHub/src/main/java/multi/dokgi/bookhub/HomeController.java
@@ -1,7 +1,5 @@
 package multi.dokgi.bookhub;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
